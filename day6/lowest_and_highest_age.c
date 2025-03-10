d5a951bc8b86a131f3585dd7435d7368b097a645
Anusha S Reddy <4q4947@gmail.com>
2025-03-10 21:30:36 +0530

    Arrays

---
+++ day6/lowest_and_highest_age.c
@@ -0,0 +1,23 @@
+//Printing highest and lowest age in the given array
+#include<stdio.h>
+int main(){
+    int age[]={23,45,67,17,89,68,23,18,67,100};
+    int length = sizeof(age) / sizeof(age[0]);
+    int lowestage=age[0];
+    int highestage=age[0];
+
+    for(int i=0; i<length;i++)
+    {
+        if(lowestage>age[i])
+        {
+            lowestage=age[i];
+        }
+        if(highestage<age[i])
+        {
+            highestage=age[i];
+        }
+    }
+    printf("The lowest age in the given array is:%d\n",lowestage);
+    printf("The highest age in the given array is:%d\n",highestage);
+    return 0;
+}
